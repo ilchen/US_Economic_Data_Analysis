9019af13f8f3726cff3fb99640861b9b32a91d15
Andrei Ilchenko <andrei.ilchenko@gmail.com>
2025-05-02 23:18:31 +0200

    Improved retrieval of average interest data from the US Treasury API

--- treasury_data/monthly_treasury_statement.py
+++ treasury_data/monthly_treasury_statement.py
@@ -1,14 +1,28 @@
+from enum import Enum
+
 import requests
 
 import pandas as pd
 from pandas.tseries.offsets import MonthBegin, MonthEnd
 
 
+class MarketableSecurityType(Enum):
+    TREASURY_NOTES = "Treasury Notes"
+    TREASURY_BONDS = "Treasury Bonds"
+    TREASURY_INFLATION_INDEXED_NOTES = "Treasury Inflation-Indexed Notes"
+    TREASURY_INFLATION_INDEXED_BONDS = "Treasury Inflation-Indexed Bonds"
+    FEDERAL_FINANCING_BANK = "Federal Financing Bank"
+    TOTAL_MARKETABLE = "Total Marketable"
+    TREASURY_BILLS = "Treasury Bills"
+    TREASURY_TIPS = "Treasury Inflation-Protected Securities (TIPS)"
+    TREASURY_FRN = "Treasury Floating Rate Notes (FRN)"
+
+
 class MTS:
     """
     Monthly Treasury Statement data from the U.S. Department of the Treasury.
     """
-
+    
     BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service'
     TABLE_9_ENDPOINT = '/v1/accounting/mts/mts_table_9'
     AVG_INTEREST_ENDPOINT = '/v2/accounting/od/avg_interest_rates'
@@ -16,11 +30,7 @@ class MTS:
     CLASSIFICATION_DESC_NET_INTEREST = 'Net Interest'
     SECURITY_TYPE_DESC_FIELD = 'security_type_desc'
     SECURITY_TYPE_DESC_MARKETABLE = 'Marketable'
-
-    MARKETABLE_SECURITY_TYPES = set(['Treasury Notes', 'Treasury Bonds', 'Treasury Inflation-Indexed Notes',
-                                     'Treasury Inflation-Indexed Bonds', 'Federal Financing Bank',
-                                     'Total Marketable', 'Treasury Bills', 'Treasury Inflation-Protected Securities (TIPS)',
-                                     'TotalMarketable', 'Treasury Floating Rate Notes (FRN)'])
+    TOTAL_MARKETABLE_ALT = "TotalMarketable"
 
     
     def __init__(self, start, end=None):
@@ -75,15 +85,21 @@ class MTS:
         """
         Retrieves the average interest that U.S. Department of the Treasury paid on marketable securities in its public debt.
     
-        :param security_type: a string designating the required security type or a list of strings
-        :returns: a pd.Series object capturing the average interest that the U.S. Treasury paind in each month 
-                  starting from 'self.start_date' and ending on 'self.end_date' including.
+        :param security_type: a MarketableSecurityType enum member or a list of MarketableSecurityType enum members
+        :returns: a pd.Series or pd.DataFrame object capturing the average interest that the U.S. Treasury paid in each month 
+                  starting from 'self.start_date' and ending on 'self.end_date' including for security types captured by
+                  the 'security_type' parameter
         """
 
         if isinstance(security_type, list):
-            assert set(list) <= MTS.MARKETABLE_SECURITY_TYPES
+            if len(security_type) == 0:
+                return pd.Series()
+            security_type = list(set(security_type))
+            assert len(security_type) <= len(MarketableSecurityType)
+            assert isinstance(security_type[0], MarketableSecurityType)
         else:
-            assert security_type in MTS.MARKETABLE_SECURITY_TYPES
+            assert isinstance(security_type, MarketableSecurityType)
+            security_type = [security_type]
 
         # Define parameters for the API request
         params = {
@@ -107,13 +123,15 @@ class MTS:
             # Convert the JSON data to a Pandas DataFrame
             df = pd.DataFrame(data['data'])
             df = df.set_index('record_date')
+            df = df.pivot(columns='security_desc', values='avg_interest_rate_amt')
+            #df = df.set_axis(pd.DatetimeIndex(df.index))
 
-            if security_type in ['Total Marketable', 'TotalMarketable']:
-                df = df.loc[df.loc[:, 'security_desc'].isin(['Total Marketable', 'TotalMarketable'])]
-                df = df.set_axis(pd.DatetimeIndex(df.index))
-                # Convert into an appropriately named pd.Series object
-                return df.loc[:, 'avg_interest_rate_amt'].rename('Total Marketable').astype('float64')
-            return df
+            if MTS.TOTAL_MARKETABLE_ALT in df.columns and MarketableSecurityType.TOTAL_MARKETABLE in security_type:
+                idx = ~df.loc[:, MTS.TOTAL_MARKETABLE_ALT].astype('float64').isna()
+                df.loc[idx, MarketableSecurityType.TOTAL_MARKETABLE.value] = df.loc[idx, MTS.TOTAL_MARKETABLE_ALT]
+
+            ret = df.loc[:, [sec.value for sec in security_type]].astype('float64').set_axis(pd.DatetimeIndex(df.index, 'ME'))
+            return ret if len(security_type) > 1 else ret.squeeze()
             
         else:
             raise KeyError(f'Date range [{start_date}; {end_date}] not present in the dataset.')
